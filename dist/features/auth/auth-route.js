168545f2123019fc78069660024bde419f31c9b4
Gledson Cruz <gledsoncruz@Gledsons-MacBook-Air.local>
2026-02-09 17:53:36 -0300

    gitignore

--- dist/features/auth/auth-route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authRoutes = void 0;
-const express_1 = require("express");
-const auth_controller_1 = require("./auth-controller");
-const router = (0, express_1.Router)();
-exports.authRoutes = router;
-router.post('/auth/login', auth_controller_1.login);
-router.post('/auth/refresh', auth_controller_1.refresh);
